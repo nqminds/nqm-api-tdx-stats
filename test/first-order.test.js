71b26eaf5478cd0e6e71db81d4ed996ad8e2edc1
Alex <mereacre@gmail.com>
2016-12-15 10:35:22 +0000

    Added test array

--- test/first-order.test.js
+++ test/first-order.test.js
@@ -31,65 +31,68 @@ const shareKeySecret = "root";
 const datasetId = "VyZFr8hWzg";
 const testFieldOne = "ecode";
 const testFieldTwo = "rate";
-const valueMinFieldOne = 201;
-const valueMinFieldTwo = 0;
 const match = {"name": "Surrey"};
-const matchMinFieldOne = 936;
-const matchMinFieldTwo = 30;
+const testOutputs = [
+  {1: 201},
+  {1: 201, 2: 0},
+  {1: 936, 2: 30},
+  {},
+  {1: {"$min": 0, "$max": 0, "$avg": 613.58}, 2: {"$min": 0, "$max": 0, "$avg": 63.42}},
+];
+const apiTimeout = 1000;
 
 describe("first-order.js", function() {
   describe(`for test dataset: ${datasetId}`, function() {
-    it(`should return the minimum for the field ${testFieldOne}`, function() {
+    it(`[1] should return the minimum for the field ${testFieldOne}`, function() {
       const api = new TDXApiStats(config);
       api.setShareKey(shareKeyID, shareKeySecret);
-      return api.getFirstOrder(["$min"], datasetId, null, [testFieldOne])
+      return api.getFirstOrder(["$min"], datasetId, null, [testFieldOne], apiTimeout)
           .then((val) => {
-            return Promise.resolve(val.data[0][testFieldOne]);
+            return Promise.resolve({1: val[testFieldOne]["$min"]});
           })
-      .should.eventually.equal(valueMinFieldOne);
+          .should.eventually.equal(testOutputs[0]);
     });
 
-    it(`should return the minimum for the field ${testFieldOne} and ${testFieldTwo}`, function() {
+    it(`[2] should return the minimum for the field ${testFieldOne} and ${testFieldTwo}`, function() {
       const api = new TDXApiStats(config);
       api.setShareKey(shareKeyID, shareKeySecret);
-      return api.getFirstOrder(["$min"], datasetId, null, [testFieldOne, testFieldTwo])
+      return api.getFirstOrder(["$min"], datasetId, null, [testFieldOne, testFieldTwo], apiTimeout)
           .then((val) => {
-            return Promise.resolve({1: val.data[0][testFieldOne], 2: val.data[0][testFieldTwo]});
+            return Promise.resolve({1: val[testFieldOne]["$min"], 2: val[testFieldTwo]["$min"]});
           })
-      .should.eventually.deep.equal({1: valueMinFieldOne, 2: valueMinFieldTwo});
+      .should.eventually.deep.equal(testOutputs[1]);
     });
 
-    it(`should return the minimum for the field ${testFieldOne} and ${testFieldOne} with the match ${JSON.stringify(match)}`, function() {
+    it(`[3] should return the minimum for the field ${testFieldOne} and ${testFieldOne} with the match ${JSON.stringify(match)}`, function() {
       const api = new TDXApiStats(config);
 
       api.setShareKey(shareKeyID, shareKeySecret);
-      return api.getFirstOrder(["$min"], datasetId, match, [testFieldOne, testFieldTwo])
+      return api.getFirstOrder(["$min"], datasetId, match, [testFieldOne, testFieldTwo], apiTimeout)
           .then((val) => {
-            return Promise.resolve({1: val.data[0][testFieldOne], 2: val.data[0][testFieldTwo]});
+            return Promise.resolve({1: val[testFieldOne]["$min"], 2: val[testFieldTwo]["$min"]});
           })
-      .should.eventually.deep.equal({1: matchMinFieldOne, 2: matchMinFieldTwo});
+      .should.eventually.deep.equal(testOutputs[2]);
     });
 
-    it("should timeout", function() {
+    it("[4] should timeout", function() {
       // set the minimum timeout
       const timeout = 1;
       const api = new TDXApiStats(config);
       api.setShareKey(shareKeyID, shareKeySecret);
       return api.getFirstOrder(["$min"], datasetId, null, [testFieldOne], timeout)
-          .then((val) => {
-            return Promise.resolve(val.data[0][testFieldOne]);
-          })
-      .should.be.rejected;
+              .should.be.rejected;
     });
 
-    it("should return the min, max and avg for the field ${testFieldOne} and ${testFieldOne}", function() {
+    it("[5] should return the min, max and avg for the field ${testFieldOne} and ${testFieldOne}", function() {
       const api = new TDXApiStats(config);
       api.setShareKey(shareKeyID, shareKeySecret);
-      return api.getFirstOrder(["$min", "$max", "$avg"], datasetId, null, [testFieldOne, testFieldTwo])
+      return api.getFirstOrder(["$min", "$max", "$avg"], datasetId, null, [testFieldOne, testFieldTwo], apiTimeout)
           .then((val) => {
-            return Promise.resolve(val.data[0]);
+            val[testFieldOne]["$avg"] = val[testFieldOne]["$avg"].toFixed(2);
+            val[testFieldTwo]["$avg"] = val[testFieldTwo]["$avg"].toFixed(2); 
+            return Promise.resolve({1: val[testFieldOne], 2: val[testFieldTwo]});
           })
-      .should.eventually.deep.equal({1: matchMinFieldOne, 2: matchMinFieldTwo});
+      .should.eventually.deep.equal(testOutputs[4]);
     });
   });
 });
