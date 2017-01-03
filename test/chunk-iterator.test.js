88444e99ded0e96a50d6d3a12213bb7aea5c21ff
Alex <mereacre@gmail.com>
2017-01-03 17:11:19 +0000

    Added next function to test

--- test/chunk-iterator.test.js
+++ test/chunk-iterator.test.js
@@ -10,7 +10,8 @@ chai.should();
 describe("chunk-iterator.test.js", function() {
   it("should return done=true", function() {
     const funcParams = ["Test1", {key1: 1, key2: 2}, 43];
-    const newFuncParams = ["Test4", {key1: 2, key2: 1}, 0];
+    const firstFuncParams = ["Test4", {key1: 2, key2: 3}, 44];
+    const secondFuncParams = ["Test4", {key1: 3, key2: 4}, 45];
 
     const callFunction = function(params) {
       const argumentList = _.map(arguments, (val, key) => (val));
@@ -19,17 +20,23 @@ describe("chunk-iterator.test.js", function() {
 
     const changeFunction = function(params) {
       const argumentList = _.map(arguments, (val, key) => (val));
+
       argumentList[0] = "Test4";
-      argumentList[1].key1 = 2;
-      argumentList[1].key2 = 1;
-      argumentList[2] = 0;
+      argumentList[1].key1 = argumentList[1].key1 + 1;
+      argumentList[1].key2 = argumentList[1].key2 + 1;
+      argumentList[2] = argumentList[2] + 1;
+
       return {done: true, arguments: argumentList};
     };
 
     const iterator = new ChunkIterator(callFunction, funcParams, changeFunction);
 
-    const ret = iterator.next();
+    let ret = iterator.next();
     ret.done.should.equal(true);
-    ret.value.should.deep.equal(newFuncParams);
+    ret.value.should.deep.equal(firstFuncParams);
+
+    ret = iterator.next();
+    ret.done.should.equal(true);
+    ret.value.should.deep.equal(secondFuncParams);
   });
 });
