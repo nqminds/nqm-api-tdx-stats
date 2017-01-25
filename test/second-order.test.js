79d1999417b40da6bcb3d11e2682dd8fef3cbbc1
Alex <mereacre@gmail.com>
2017-01-25 16:11:57 +0000

    Added getBasicBins

--- test/second-order.test.js
+++ test/second-order.test.js
@@ -30,6 +30,33 @@ const testInputs = [
       upp: [10, 20],
     },
   }, // Test [1]
+  {
+    match: {},
+    field: "BayCount",
+    binIndex: {
+      type: "number",
+      low: [1],
+      upp: [20],
+    },
+  }, // Test [2]
+  {
+    match: {},
+    field: "BayCount",
+    binIndex: {
+      type: "number",
+      low: [],
+      upp: [],
+    },
+  }, // Test [2]
+  {
+    match: {},
+    field: "",
+    binIndex: {
+      type: "number",
+      low: [1],
+      upp: [20],
+    },
+  }, // Test [3]
 ];
 
 const testOutputs = [
@@ -37,6 +64,18 @@ const testOutputs = [
     count: 21,
     bins: [10, 10],
   }, // Test [1]
+  {
+    count: 21,
+    bins: [20],
+  }, // Test [2]
+  {
+    count: 21,
+    bins: [],
+  }, // Test [3]
+  {
+    count: 21,
+    bins: [],
+  }, // Test [4]
 ];
 
 const testTimeout = 20000;
@@ -45,7 +84,7 @@ const apiTimeout = 10000;
 describe.only("second-order.js", function() {
   this.timeout(testTimeout);
 
-  it(`should return the histogram for binIndex ${testInputs[0].binIndex}`, function() {
+  it(`should return the histogram for binIndex ${JSON.stringify(testInputs[0].binIndex)}`, function() {
     const test = 0;
 
     const api = new TDXApiStats(configNqm);
@@ -53,7 +92,7 @@ describe.only("second-order.js", function() {
 
     const params = {
       match: testInputs[test].match,
-      fields: testInputs[test].field,
+      field: testInputs[test].field,
       binIndex: testInputs[test].binIndex,
       index: [],
       limit: 0,
@@ -63,4 +102,61 @@ describe.only("second-order.js", function() {
     return api.getBasicBins(datasetIdNqm, params)
         .should.eventually.deep.equal(testOutputs[test]);
   });
+
+  it(`should return the histogram for binIndex ${JSON.stringify(testInputs[1].binIndex)}`, function() {
+    const test = 1;
+
+    const api = new TDXApiStats(configNqm);
+    api.setShareKey(shareKeyIDNqm, shareKeySecretNqm);
+
+    const params = {
+      match: testInputs[test].match,
+      field: testInputs[test].field,
+      binIndex: testInputs[test].binIndex,
+      index: [],
+      limit: 0,
+      timeout: apiTimeout,
+    };
+
+    return api.getBasicBins(datasetIdNqm, params)
+        .should.eventually.deep.equal(testOutputs[test]);
+  });
+
+  it(`should return the histogram for binIndex ${JSON.stringify(testInputs[2].binIndex)}`, function() {
+    const test = 2;
+
+    const api = new TDXApiStats(configNqm);
+    api.setShareKey(shareKeyIDNqm, shareKeySecretNqm);
+
+    const params = {
+      match: testInputs[test].match,
+      field: testInputs[test].field,
+      binIndex: testInputs[test].binIndex,
+      index: [],
+      limit: 0,
+      timeout: apiTimeout,
+    };
+
+    return api.getBasicBins(datasetIdNqm, params)
+        .should.eventually.deep.equal(testOutputs[test]);
+  });
+
+  it(`should return the histogram for binIndex ${JSON.stringify(testInputs[3].binIndex)}`, function() {
+    const test = 3;
+
+    const api = new TDXApiStats(configNqm);
+    api.setShareKey(shareKeyIDNqm, shareKeySecretNqm);
+
+    const params = {
+      match: testInputs[test].match,
+      field: testInputs[test].field,
+      binIndex: testInputs[test].binIndex,
+      index: [],
+      limit: 0,
+      timeout: apiTimeout,
+    };
+
+    return api.getBasicBins(datasetIdNqm, params)
+        .should.eventually.deep.equal(testOutputs[test]);
+  });  
 });
