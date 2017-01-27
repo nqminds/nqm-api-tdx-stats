59b5139d7fef325f6174752be31b11ba8f5a7514
Alex <mereacre@gmail.com>
2017-01-27 16:27:18 +0000

    Added tests

--- test/secon-order-chunk.test.js
+++ test/secon-order-chunk.test.js
@@ -46,6 +46,17 @@ const testInputs = [
     },
     chunkSize: 10,
   }, // Test [2]
+  {
+    match: {"$and": [{"SID": "2021"}, {"Waste_Type": "WOODMX"}, {"HWRC": "Winchester"}]},
+    field: "Friday",
+    index: ["SID", "HWRC", "Waste_Type", "NID", "Contract", "First_Movement"],
+    binIndex: {
+      type: "number",
+      low: [0.41, 9.687000000000001, 18.964000000000002, 28.241000000000003, 37.518, 46.795, 56.072, 65.349, 74.626, 83.903],
+      upp: [9.687000000000001, 18.964000000000002, 28.241000000000003, 37.518, 46.795, 56.072, 65.349, 74.626, 83.903, 93.18],
+    },
+    chunkSize: 10,
+  }, // Test [3]
 ];
 
 const testOutputs = [
@@ -69,12 +80,22 @@ const testOutputs = [
       upp: [9.687000000000001, 18.964000000000002, 28.241000000000003, 37.518, 46.795, 56.072, 65.349, 74.626, 83.903, 93.18],
     },
   }, // Test [2]
+  {
+    count: 50,
+    bins: [30, 0, 0, 10, 0, 0, 0, 0, 0, 10],
+    binIndex: {
+      type: "number",
+      count: 10,
+      low: [0.41, 9.687000000000001, 18.964000000000002, 28.241000000000003, 37.518, 46.795, 56.072, 65.349, 74.626, 83.903],
+      upp: [9.687000000000001, 18.964000000000002, 28.241000000000003, 37.518, 46.795, 56.072, 65.349, 74.626, 83.903, 93.18],
+    },
+  }, // Test [3]
 ];
 
 const testTimeout = 20000;
 const apiTimeout = 10000;
 
-describe.only("second-order-chunk.js", function() {
+describe("second-order-chunk.js", function() {
   this.timeout(testTimeout);
 
   it(`should return the histogram for binIndex ${JSON.stringify(testInputs[0].binIndex)}`, function() {
@@ -98,6 +119,25 @@ describe.only("second-order-chunk.js", function() {
 
   it(`should return the histogram for binIndex ${JSON.stringify(testInputs[1].binIndex)}`, function() {
     const test = 1;
+
+    const api = new TDXApiStats(config);
+    api.setShareKey(shareKeyID, shareKeySecret);
+
+    const params = {
+      match: testInputs[test].match,
+      field: testInputs[test].field,
+      index: testInputs[test].index,
+      binIndex: testInputs[test].binIndex,
+      chunkSize: testInputs[test].chunkSize,
+      timeout: apiTimeout,
+    };
+
+    return api.getHistogramChunk(datasetId, params)
+        .should.eventually.deep.equal(testOutputs[test]);
+  });
+
+  it(`should return the histogram for binIndex ${JSON.stringify(testInputs[2].binIndex)}`, function() {
+    const test = 2;
 
     const api = new TDXApiStats(config);
     api.setShareKey(shareKeyID, shareKeySecret);
